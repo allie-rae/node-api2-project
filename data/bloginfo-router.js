a0e81c3d4cdd49195e407a030bb0e400b3e3ffdb
Allie Robinson <allierob93@gmail.com>
2020-01-15 02:20:06 -0600

    POST request for /api/posts finished

--- data/bloginfo-router.js
+++ data/bloginfo-router.js
@@ -1,15 +1,34 @@
 const router = require('express').Router();
 const Blog = require('./db');
 
+router.post('/', (req, res) => {
+    post = req.body;
+    title = req.body.title;
+    contents = req.body.contents;
+    console.log(post)
+
+    !title || !contents ?
+    res.status(400).json({ errorMessage: "Please provide a title and contents for the post."})
+    :
+    Blog.insert(post)
+        .then(id => {
+            res.status(201).json(post);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: "There was an error while saving the post to the database."});
+        });
+});
+
 router.get('/', (req, res) => {
     Blog.find()
-    .then(posts => {
-        res.status(200).json(posts);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ errorMessage: "The posts information could not be retrieved."})
-    })
-})
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: "The posts information could not be retrieved." });
+        });
+});
 
 module.exports = router;
