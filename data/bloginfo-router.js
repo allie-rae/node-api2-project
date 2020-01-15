c74ea4ce3328a4bedb6b3f7695ccb03a0b133845
Allie Robinson <allierob93@gmail.com>
2020-01-15 11:40:26 -0600

    GET post by id finished

--- data/bloginfo-router.js
+++ data/bloginfo-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Blog = require('./db');
 
+// POST Blog 
 router.post('/', (req, res) => {
     post = req.body;
     title = req.body.title;
@@ -20,6 +21,7 @@ router.post('/', (req, res) => {
             });
 });
 
+// POST Comment 
 router.post('/:id/comments', (req, res) => {
     req.body.post_id = req.params.id;
     let text = req.body.text;
@@ -30,17 +32,19 @@ router.post('/:id/comments', (req, res) => {
         :
         Blog.insertComment(comment)
             .then(id => {
+                console.log(id)
                 !id ?
                     res.status(404).json({ errorMessage: "The post with the specified ID does not exist." })
                     :
                     res.status(201).json(id);
-                    // Blog.findCommentById(req.body.post_id)
-                    //     .then(comment => {
-                    //         res.status(200).json(comment);
-                    //     })
-                    //     .catch(err => {
-                    //         console.log(err);
-                    //     });
+                // Blog.findCommentById(id)
+                //     .then(comment => {
+                //         console.log(comment)
+                //         res.status(200).json(comment);
+                //     })
+                //     .catch(err => {
+                //         console.log(err);
+                //     });
             })
             .catch(err => {
                 console.log(err);
@@ -48,6 +52,7 @@ router.post('/:id/comments', (req, res) => {
             });
 });
 
+// GET all posts 
 router.get('/', (req, res) => {
     Blog.find()
         .then(posts => {
@@ -56,6 +61,22 @@ router.get('/', (req, res) => {
         .catch(err => {
             console.log(err);
             res.status(500).json({ errorMessage: "The posts information could not be retrieved." });
+        });
+});
+
+// GET post by id
+router.get('/:id', (req, res) => {
+    let id = req.params.id
+    Blog.findById(id)
+        .then(array => {
+            !array[0] ? 
+            res.status(404).json({ errorMessage: "The post with the specified ID does not exist." })
+            :
+            res.status(200).json(array[0]);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: "The post information could not be retrieved." });
         });
 });
 
