45336bd2a86ae90c3fd74c1e5f0ae8836c44a808
Allie Robinson <allierob93@gmail.com>
2020-01-15 03:07:58 -0600

    POST request for comments working. Not entirely sure if I did it corrently. Will look over it again tomorrow.

--- data/bloginfo-router.js
+++ data/bloginfo-router.js
@@ -8,16 +8,44 @@ router.post('/', (req, res) => {
     console.log(post)
 
     !title || !contents ?
-    res.status(400).json({ errorMessage: "Please provide a title and contents for the post."})
-    :
-    Blog.insert(post)
-        .then(id => {
-            res.status(201).json(post);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ errorMessage: "There was an error while saving the post to the database."});
-        });
+        res.status(400).json({ errorMessage: "Please provide a title and contents for the post." })
+        :
+        Blog.insert(post)
+            .then(id => {
+                res.status(201).json(post);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ errorMessage: "There was an error while saving the post to the database." });
+            });
+});
+
+router.post('/:id/comments', (req, res) => {
+    req.body.post_id = req.params.id;
+    let text = req.body.text;
+    let comment = req.body;
+    console.log(comment)
+    !text ?
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+        :
+        Blog.insertComment(comment)
+            .then(id => {
+                !id ?
+                    res.status(404).json({ errorMessage: "The post with the specified ID does not exist." })
+                    :
+                    res.status(201).json(id);
+                    // Blog.findCommentById(req.body.post_id)
+                    //     .then(comment => {
+                    //         res.status(200).json(comment);
+                    //     })
+                    //     .catch(err => {
+                    //         console.log(err);
+                    //     });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ errorMessage: "There was an error while saving the comment to the database." });
+            });
 });
 
 router.get('/', (req, res) => {
